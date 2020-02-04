247900223f2aed7a4fda6acad3bcec2593d89446
Monteiro <monteiro.lcm@gmail.com>
2020-02-04 00:54:33 +0000

    added senario 2

--- dispatcher/main.cpp
+++ dispatcher/main.cpp
@@ -8,7 +8,9 @@
  **
  * std
  */
+#include <memory>
 #include <iostream>
+#include <functional>
 /**
  * local
  */
@@ -27,25 +29,66 @@ public:
         return i + i;
     }
 };
+
+class Callable {
+public:
+    virtual int operator()(int i) = 0;
+};
+class Callable1: public Callable {
+    int operator()(int i){
+        return i * 10;
+    }
+};
+class Callable2: public Callable {
+    int operator()(int i){
+        return i * 10;
+    }
+};
+
 int main() {   
     using namespace std::placeholders;
-    using Dispatcher = Helper::SDispatcher<std::string, std::function<int(int)>>;
-
-    Dispatcher d;
     /**
-     * register
+     * --------------------------------------------------------------------------
+     * type 1
+     * --------------------------------------------------------------------------
      */
-    Process p;
-    d.add_callable("process" , std::bind(&Process::run, &p, _1));
-    d.add_callable("lambda"  , [](int i){ return i; });
-    d.add_callable("function", &runable);
+    {
+        using Dispatcher = Helper::SDispatcher<std::string, std::function<int(int)>>;
+        Dispatcher d;
+        /**
+         * register
+         */
+        Process p;
+        d.add_callable("process" , std::bind(&Process::run, &p, _1));
+        d.add_callable("lambda"  , [](int i){ return i; });
+        d.add_callable("function", &runable);
+        /**
+         * invoke
+         */
+        std::cout << d.invoke("process" , 3) << std::endl;
+        std::cout << d.invoke("lambda"  , 3) << std::endl;
+        std::cout << d.invoke("function", 3) << std::endl;
+    }
     /**
-     * invoke
+     * --------------------------------------------------------------------------
+     * type 2
+     * --------------------------------------------------------------------------
      */
-    std::cout << d.invoke("process" , 3) << std::endl;
-    std::cout << d.invoke("lambda"  , 3) << std::endl;
-    std::cout << d.invoke("function", 3) << std::endl;
-
+    // {
+    //     using Dispatcher = Helper::SDispatcher<
+    //         std::string, std::shared_ptr<Callable>>;
+    //     Dispatcher d;
+    //     /**
+    //      * register
+    //      */
+    //     d.add_callable("Callable2" , std::make_shared<Callable2>());
+    //     d.add_callable("Callable2" , std::make_shared<Callable2>());
+    //     /* 
+    //      * invoke
+    //      */
+    //     std::cout << d.invoke("Callable2", 3) << std::endl;
+    //     std::cout << d.invoke("Callable2", 3) << std::endl;
+    // }
     return 0;
 }
 /**
